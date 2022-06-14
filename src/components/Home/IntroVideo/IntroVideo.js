7f8373210ef2da96dda7e3e810d93256356b6576
MK-Khan123 <mehnazkhan231@gmail.com>
2022-06-15 00:28:10 +0600

    IntroVideo component framework done

--- src/components/Home/IntroVideo/IntroVideo.js
+++ src/components/Home/IntroVideo/IntroVideo.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Box, Container, Typography } from '@mui/material';
 
 const IntroVideo = () => {
+
+    const introVideo = "https://www.youtube.com/embed/tgbNymZ7vqY?controls=0";
+
     return (
-        <div>
-            This is glance video
-        </div>
+        <Container id='intro-video' sx={{ paddingBottom: 5, borderBottom: '3px solid #970C0C' }}>
+            <Typography mt={12} mb={6} textTransform='uppercase' textAlign='center' fontWeight='bold' variant="h3" gutterBottom component="div">
+                A glance at our product
+            </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <iframe
+                    title="A glance at our product"
+                    width="80%"
+                    height="400"
+                    src={`${introVideo}`}>
+                </iframe>
+            </Box>
+        </Container>
     );
 };
 
